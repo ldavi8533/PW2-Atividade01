b1f6a51adb0eb05ccdfdc0634238f76e03d9e052
ldavi8533 <75754211+ldavi8533@users.noreply.github.com>
2023-03-23 16:40:43 -0300

    Create exercicio.ts

---
+++ exercicio.ts
@@ -0,0 +1,44 @@
+
+let employee = {
+    code: 10,
+    name: 'jhon'
+}
+
+employee.code = 10;
+
+employee.name = "John";
+
+type Ipessoa = {
+    nome: string
+    idade: number | string
+    profissao: string
+}
+
+let pessoa1: Ipessoa = {
+    nome: 'maria',
+    idade: 29,
+    profissao: 'atriz',
+}
+
+let pessoa2: Ipessoa = {
+    nome: "roberto",
+    idade: 19,
+    profissao: "Padeiro",
+}
+
+let pessoa3: Ipessoa = {
+
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+
+};
+
+
+let pessoa4: Ipessoa = {
+
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+
+}
